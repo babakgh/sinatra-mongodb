52cb669089a46bf1529f676ba74f6a0a60741d57
Philip Källberg <philip@cloud66.com>
2013-11-14 11:20:18 +0000

    Update thingsweshoulddo.rb

--- thingsweshoulddo.rb
+++ thingsweshoulddo.rb
@@ -7,9 +7,9 @@ class ThingsWeShouldDo < Sinatra::Base
   set :google_analytics_id, ''
   ######################################################
 
-  if ENV['MONGOHQ_URL']
-    uri = URI.parse(ENV['MONGOHQ_URL'])
-    MongoMapper.connection = Mongo::Connection.from_uri(ENV['MONGOHQ_URL'])
+  if ENV['MONGODB_URL']
+    uri = URI.parse(ENV['MONGODB_URL'])
+    MongoMapper.connection = Mongo::Connection.from_uri(ENV['MONGODB_URL'])
     MongoMapper.database = uri.path.gsub(/^\//, '')
   else
     MongoMapper.database = 'things'
